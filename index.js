622b5806615e877114ff8edcb114a6d8fc51a80f
Dustin Jackson <dustinhja@uw.edu>
2021-07-30 11:21:26 -0700

    kaboom is fully operational! hopping over to pagination then doing the fun stuff

---
+++ index.js
@@ -0,0 +1,8 @@
+import kaboom, {Vec2} from 'kaboom';
+
+const k = kaboom({
+    global: true,
+    fullscreen: true,
+    scale: 1,
+    debug: true,
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# zeldaNestor
+web based game to learn how to integrate with Nestor PHP framework
